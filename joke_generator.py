0fa4499f264a4828655abe122ec7334f56a2fd4e
Ritesh Ramchandani <riteshramchandani123@gmail.com>
2024-09-08 00:02:39 +0530

    Updated joke_generator.py file. Added Ollama dependency to serve the POC of joke_generator.py

--- joke_generator.py
+++ joke_generator.py
@@ -14,6 +14,7 @@ from llama_index.core.workflow import (
 from llama_index.llms.huggingface import HuggingFaceLLM
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 from langchain_huggingface.llms.huggingface_pipeline import HuggingFacePipeline
+from llama_index.llms.ollama import Ollama
 
 HUGGING_FACE_TOKEN = os.getenv('HF_TOKEN')
 
@@ -25,18 +26,19 @@ class JokeEvent(Event):
 class JokeFlow(Workflow):
     # llm = HuggingFaceLLM(model_name='meta-llama/Meta-Llama-3.1-8B',
     #                      tokenizer_name='meta-llama/Meta-Llama-3.1-8B', device_map='auto')
-    model = AutoModelForCausalLM.from_pretrained(pretrained_model_name_or_path='StabilityAI/stablelm-tuned-alpha-3b', torch_dtype=torch.float16).to('mps')
-    tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path='StabilityAI/stablelm-tuned-alpha-3b').to('mps')
-    hf_pipeline = pipeline(task='text-generation', model=model, tokenizer=tokenizer, torch_dtype=torch.float16,
-                           device='mps')
-    llm = HuggingFacePipeline(pipeline=hf_pipeline)
+    # model = AutoModelForCausalLM.from_pretrained(pretrained_model_name_or_path='StabilityAI/stablelm-tuned-alpha-3b', torch_dtype=torch.float16).to('mps')
+    # tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path='StabilityAI/stablelm-tuned-alpha-3b').to('mps')
+    # hf_pipeline = pipeline(task='text-generation', model=model, tokenizer=tokenizer, torch_dtype=torch.float16,
+    #                        device='mps')
+    # llm = HuggingFacePipeline(pipeline=hf_pipeline)
+    llm = Ollama(model='llama2')
 
     @step
     async def generate_joke(self, ev: StartEvent) -> JokeEvent:
         topic = ev.topic
 
         prompt = f"Write your best joke about {topic}."
-        response = await self.llm.ainvoke(prompt)
+        response = await self.llm.acomplete(prompt)
         return JokeEvent(joke=str(response))
 
     @step
@@ -44,7 +46,7 @@ class JokeFlow(Workflow):
         joke = ev.joke
 
         prompt = f"Give a thorough analysis and critique of the following joke: {joke}"
-        response = await self.llm.ainvoke(prompt)
+        response = await self.llm.acomplete(prompt)
         return StopEvent(result=str(response))
 
 
