d0c9fc032e1ec5b6adec1a51aaf5fab7b0443150
Ritesh Ramchandani <riteshramchandani123@gmail.com>
2024-09-07 23:20:43 +0530

    Updated joke_generator.py file. Loaded LLM model into Macbook pro GPU and added HuggingFacePipeline from langchain as llama index was giving issues.

--- joke_generator.py
+++ joke_generator.py
@@ -1,5 +1,6 @@
 import os
 
+import torch
 from llama_index.core.workflow import (
     Event,
     StartEvent,
@@ -11,7 +12,8 @@ from llama_index.core.workflow import (
 # `pip install llama-index-llms-openai` if you don't already have it
 # from llama_index.llms.openai import OpenAI
 from llama_index.llms.huggingface import HuggingFaceLLM
-
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from langchain_huggingface.llms.huggingface_pipeline import HuggingFacePipeline
 
 HUGGING_FACE_TOKEN = os.getenv('HF_TOKEN')
 
@@ -21,15 +23,20 @@ class JokeEvent(Event):
 
 
 class JokeFlow(Workflow):
-    llm = HuggingFaceLLM(model_name='meta-llama/Meta-Llama-3.1-8B',
-                         tokenizer_name='meta-llama/Meta-Llama-3.1-8B')
+    # llm = HuggingFaceLLM(model_name='meta-llama/Meta-Llama-3.1-8B',
+    #                      tokenizer_name='meta-llama/Meta-Llama-3.1-8B', device_map='auto')
+    model = AutoModelForCausalLM.from_pretrained(pretrained_model_name_or_path='StabilityAI/stablelm-tuned-alpha-3b', torch_dtype=torch.float16).to('mps')
+    tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path='StabilityAI/stablelm-tuned-alpha-3b').to('mps')
+    hf_pipeline = pipeline(task='text-generation', model=model, tokenizer=tokenizer, torch_dtype=torch.float16,
+                           device='mps')
+    llm = HuggingFacePipeline(pipeline=hf_pipeline)
 
     @step
     async def generate_joke(self, ev: StartEvent) -> JokeEvent:
         topic = ev.topic
 
         prompt = f"Write your best joke about {topic}."
-        response = await self.llm.acomplete(prompt)
+        response = await self.llm.ainvoke(prompt)
         return JokeEvent(joke=str(response))
 
     @step
@@ -37,7 +44,7 @@ class JokeFlow(Workflow):
         joke = ev.joke
 
         prompt = f"Give a thorough analysis and critique of the following joke: {joke}"
-        response = await self.llm.acomplete(prompt)
+        response = await self.llm.ainvoke(prompt)
         return StopEvent(result=str(response))
 
 
